ee1e84cbd13790512be1d3897dd60328c6e134c2
Reid Ransom <reid@reidransom.com>
2017-12-11 14:46:23 -0500

    consistent ordering of checks in page view

--- src/heartbeat/util.py
+++ src/heartbeat/util.py
@@ -9,14 +9,14 @@ from .settings import HEARTBEAT
 def get_checks(request):
     """ Return dict with all the checkers data
     """
-    result = {}
+    results = []
     for check in HEARTBEAT['checkers']:
         check_fn = import_string(check)
         checker_name = check_fn.__name__.split('.')[-1]
         checker_name = checker_name.replace('_', ' ')  # replace underscore
         data = get_check(check_fn, request)
-        result.update({checker_name: data})
-    return result
+        results.append(dict(data, name=checker_name))
+    return results
 
 def get_check(check_fn, request):
     """ Get checks for checker, pass/fail and data in JSON
@@ -43,20 +43,19 @@ def get_check(check_fn, request):
     }
 
 def get_status(request):
-    result = get_checks(request)
+    results = get_checks(request)
 
     # check that everything is passing
-    checks_passed = 0
-    for check, check_dict in result.items():
-        if check_dict['pass'] == True:
-            checks_passed += 1
-    lcms_pass = True if checks_passed == len(result) else False
+    checks_passed = True
+    for check_dict in results:
+        if not check_dict['pass']:
+            checks_passed = False
     lcms_status = {
         'label': 'The LCMS is up and running.',
-        'pass': lcms_pass
+        'pass': checks_passed,
     }
 
     return {
-        'result': result,
+        'results': results,
         'status': lcms_status,
     }
